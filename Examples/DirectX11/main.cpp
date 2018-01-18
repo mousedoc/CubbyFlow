458f2e9d6f8090fb4119eb1e088de990a7cbc5bd
김동민 <dmk98@naver.com>
2018-01-18 18:18:28 +0900

    Update DirectX11 LevelSetSim

--- Examples/DirectX11/main.cpp
+++ Examples/DirectX11/main.cpp
@@ -29,6 +29,7 @@
 #include <Solver/LevelSet/LevelSetLiquidSolver3.h>
 #include <Surface/Implicit/ImplicitSurfaceSet3.h>
 #include <Utils/Logger.h>
+#include <Utils/Timer.h>
 
 #include <pystring/pystring.h>
 
@@ -82,13 +83,19 @@ ID3D11RenderTargetView* g_pRenderTargetView = NULL;
 ID3D11VertexShader*     g_pVertexShader = NULL;
 ID3D11PixelShader*      g_pPixelShader = NULL;
 ID3D11InputLayout*      g_pVertexLayout = NULL;
-ID3D11Buffer*           g_pVertexBuffer = NULL;
-ID3D11Buffer*           g_pIndexBuffer = NULL;
 ID3D11Buffer*           g_pConstantBuffer = NULL;
 XMMATRIX                g_World;
 XMMATRIX                g_View;
 XMMATRIX                g_Projection;
+XMVECTOR				g_Eye =	XMVectorSet(0.f, 3.0f, -4.8f, 0.0f);
 
+
+ID3D11Buffer*           g_pVertexBuffer = NULL;
+ID3D11Buffer*           g_pIndexBuffer = NULL;
+ID3D11RasterizerState*	g_pRasterizerState = NULL;
+
+TriangleMesh3			g_mesh;
+int						g_frameCnt = 0;
 
 //--------------------------------------------------------------------------------------
 // Forward declarations
@@ -98,7 +105,6 @@ HRESULT InitDevice();
 void CleanupDevice();
 LRESULT CALLBACK    WndProc( HWND, UINT, WPARAM, LPARAM );
 void Render();
-
 
 //--------------------------------------------------------------------------------------
 // Entry point to the program. Initializes everything and goes into a message processing 
@@ -165,7 +171,7 @@ HRESULT InitWindow( HINSTANCE hInstance, int nCmdShow )
     g_hInst = hInstance;
     RECT rc = { 0, 0, 640, 480 };
     AdjustWindowRect( &rc, WS_OVERLAPPEDWINDOW, FALSE );
-    g_hWnd = CreateWindow( L"TutorialWindowClass", L"Direct3D 11 Tutorial 4: 3D Spaces", WS_OVERLAPPEDWINDOW,
+    g_hWnd = CreateWindow( L"TutorialWindowClass", L"LevelSetLiquidSim", WS_OVERLAPPEDWINDOW,
                            CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, NULL, NULL, hInstance,
                            NULL );
     if( !g_hWnd )
@@ -208,12 +214,12 @@ HRESULT CompileShaderFromFile( WCHAR* szFileName, LPCSTR szEntryPoint, LPCSTR sz
     return S_OK;
 }
 
-
 //--------------------------------------------------------------------------------------
 // Create Direct3D device and swap chain
 //--------------------------------------------------------------------------------------
 HRESULT InitDevice()
 {
+
     HRESULT hr = S_OK;
 
     RECT rc;
@@ -343,62 +349,48 @@ HRESULT InitDevice()
         return hr;
 
     // Create vertex buffer
-    SimpleVertex vertices[] =
-    {
-        { XMFLOAT3( -1.0f, 1.0f, -1.0f ), XMFLOAT4( 0.0f, 0.0f, 1.0f, 1.0f ) },
-        { XMFLOAT3( 1.0f, 1.0f, -1.0f ), XMFLOAT4( 0.0f, 1.0f, 0.0f, 1.0f ) },
-        { XMFLOAT3( 1.0f, 1.0f, 1.0f ), XMFLOAT4( 0.0f, 1.0f, 1.0f, 1.0f ) },
-        { XMFLOAT3( -1.0f, 1.0f, 1.0f ), XMFLOAT4( 1.0f, 0.0f, 0.0f, 1.0f ) },
-        { XMFLOAT3( -1.0f, -1.0f, -1.0f ), XMFLOAT4( 1.0f, 0.0f, 1.0f, 1.0f ) },
-        { XMFLOAT3( 1.0f, -1.0f, -1.0f ), XMFLOAT4( 1.0f, 1.0f, 0.0f, 1.0f ) },
-        { XMFLOAT3( 1.0f, -1.0f, 1.0f ), XMFLOAT4( 1.0f, 1.0f, 1.0f, 1.0f ) },
-        { XMFLOAT3( -1.0f, -1.0f, 1.0f ), XMFLOAT4( 0.0f, 0.0f, 0.0f, 1.0f ) },
-    };
-    D3D11_BUFFER_DESC bd;
-	ZeroMemory( &bd, sizeof(bd) );
-    bd.Usage = D3D11_USAGE_DEFAULT;
-    bd.ByteWidth = sizeof( SimpleVertex ) * 8;
-    bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
-	bd.CPUAccessFlags = 0;
-    D3D11_SUBRESOURCE_DATA InitData;
-	ZeroMemory( &InitData, sizeof(InitData) );
-    InitData.pSysMem = vertices;
-    hr = g_pd3dDevice->CreateBuffer( &bd, &InitData, &g_pVertexBuffer );
-    if( FAILED( hr ) )
-        return hr;
 
-    // Set vertex buffer
-    UINT stride = sizeof( SimpleVertex );
-    UINT offset = 0;
-    g_pImmediateContext->IASetVertexBuffers( 0, 1, &g_pVertexBuffer, &stride, &offset );
+	D3D11_BUFFER_DESC bufDesc;
 
-    // Create index buffer
-    WORD indices[] =
-    {
-        3,1,0,
-        2,1,3,
+	ZeroMemory(&bufDesc, sizeof(bufDesc));
+	bufDesc.Usage = D3D11_USAGE_DYNAMIC;
+	bufDesc.ByteWidth = sizeof(SimpleVertex) * 10000;
+	bufDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	bufDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	bufDesc.MiscFlags = 0;
+	bufDesc.StructureByteStride = 0;
 
-        0,5,4,
-        1,5,0,
+	SimpleVertex* liquidVertices = new SimpleVertex[10000];
+	WORD* indices = new WORD[50000];
 
-        3,4,7,
-        0,4,3,
+	D3D11_SUBRESOURCE_DATA vBufferData;
+	vBufferData.pSysMem = liquidVertices;
+	vBufferData.SysMemPitch = 0;
+	vBufferData.SysMemSlicePitch = 0;
 
-        1,6,5,
-        2,6,1,
+	hr = g_pd3dDevice->CreateBuffer(&bufDesc, &vBufferData, &g_pVertexBuffer);
+	if (FAILED(hr))
+		return hr;
+	UINT stride = sizeof(SimpleVertex);
+	UINT offset = 0;
+	g_pImmediateContext->IASetVertexBuffers(0, 1, &g_pVertexBuffer, &stride, &offset);
 
-        2,7,6,
-        3,7,2,
+	D3D11_BUFFER_DESC ibufDesc;
+	
+	ibufDesc.Usage = D3D11_USAGE_DYNAMIC;
+	ibufDesc.ByteWidth = sizeof(WORD) * 50000;
+	ibufDesc.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	ibufDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	ibufDesc.MiscFlags = 0;
+	ibufDesc.StructureByteStride = 0;
+	
+	D3D11_SUBRESOURCE_DATA iBufferData;
+	
+	iBufferData.pSysMem = indices;
+	iBufferData.SysMemPitch = 0;
+	iBufferData.SysMemSlicePitch = 0;
 
-        6,4,5,
-        7,4,6,
-    };
-    bd.Usage = D3D11_USAGE_DEFAULT;
-    bd.ByteWidth = sizeof( WORD ) * 36;        // 36 vertices needed for 12 triangles in a triangle list
-    bd.BindFlags = D3D11_BIND_INDEX_BUFFER;
-	bd.CPUAccessFlags = 0;
-    InitData.pSysMem = indices;
-    hr = g_pd3dDevice->CreateBuffer( &bd, &InitData, &g_pIndexBuffer );
+    hr = g_pd3dDevice->CreateBuffer( &ibufDesc, &iBufferData, &g_pIndexBuffer );
     if( FAILED( hr ) )
         return hr;
 
@@ -409,11 +401,11 @@ HRESULT InitDevice()
     g_pImmediateContext->IASetPrimitiveTopology( D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST );
 
 	// Create the constant buffer
-	bd.Usage = D3D11_USAGE_DEFAULT;
-	bd.ByteWidth = sizeof(ConstantBuffer);
-	bd.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
-	bd.CPUAccessFlags = 0;
-    hr = g_pd3dDevice->CreateBuffer( &bd, NULL, &g_pConstantBuffer );
+	bufDesc.Usage = D3D11_USAGE_DEFAULT;
+	bufDesc.ByteWidth = sizeof(ConstantBuffer);
+	bufDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	bufDesc.CPUAccessFlags = 0;
+    hr = g_pd3dDevice->CreateBuffer( &bufDesc, NULL, &g_pConstantBuffer );
     if( FAILED( hr ) )
         return hr;
 
@@ -421,13 +413,26 @@ HRESULT InitDevice()
 	g_World = XMMatrixIdentity();
 
     // Initialize the view matrix
-	XMVECTOR Eye = XMVectorSet( 0.0f, 1.0f, -5.0f, 0.0f );
+	XMVECTOR Eye = XMVectorSet( 0.0f, 3.0f, -5.0f, 0.0f );
 	XMVECTOR At = XMVectorSet( 0.0f, 1.0f, 0.0f, 0.0f );
 	XMVECTOR Up = XMVectorSet( 0.0f, 1.0f, 0.0f, 0.0f );
 	g_View = XMMatrixLookAtLH( Eye, At, Up );
 
     // Initialize the projection matrix
 	g_Projection = XMMatrixPerspectiveFovLH( XM_PIDIV2, width / (FLOAT)height, 0.01f, 100.0f );
+
+	D3D11_RASTERIZER_DESC RSDesc;
+	RSDesc.FillMode = D3D11_FILL_SOLID;
+	RSDesc.CullMode = D3D11_CULL_NONE; 
+	RSDesc.DepthBias = 0;                      
+	RSDesc.DepthBiasClamp = 0;
+	RSDesc.SlopeScaledDepthBias = 0;
+	RSDesc.DepthClipEnable = FALSE;            
+	RSDesc.ScissorEnable = FALSE;             
+	RSDesc.MultisampleEnable = FALSE;         
+	RSDesc.AntialiasedLineEnable = FALSE;        
+
+	g_pd3dDevice->CreateRasterizerState(&RSDesc, &g_pRasterizerState);
 
     return S_OK;
 }
@@ -463,6 +468,15 @@ LRESULT CALLBACK WndProc( HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam 
 
     switch( message )
     {
+		case WM_KEYDOWN:
+			switch (wParam) {
+			case 'W':
+				g_Eye += XMVectorSet(1, 0, 0, 0);
+				break;
+			case 'S':
+				g_Eye += XMVectorSet(-1, 0, 0, 0);
+				break;
+			}
         case WM_PAINT:
             hdc = BeginPaint( hWnd, &ps );
             EndPaint( hWnd, &ps );
@@ -479,36 +493,94 @@ LRESULT CALLBACK WndProc( HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam 
     return 0;
 }
 
+void UpdateBuffer() {
+	D3D11_MAPPED_SUBRESOURCE mappedVertexResource, mappedIndexResource;
+	ZeroMemory(&mappedVertexResource, sizeof(D3D11_MAPPED_SUBRESOURCE));
+	ZeroMemory(&mappedIndexResource, sizeof(D3D11_MAPPED_SUBRESOURCE));
+
+	int numberOfPoints = g_mesh.NumberOfPoints();
+
+	SimpleVertex *vertices = new SimpleVertex[numberOfPoints];
+
+	for (int i = 0; i < numberOfPoints; i++) {
+		vertices[i].Pos.x = g_mesh.Point(i).x;
+		vertices[i].Pos.y = g_mesh.Point(i).y;
+		vertices[i].Pos.z = g_mesh.Point(i).z;
+		vertices[i].Color = { 0.52f, 0.80f, 0.92f, 1.0f };
+	}
+
+	int numberOfTriangles = g_mesh.NumberOfTriangles();
+	WORD *indices = new WORD[3 * numberOfTriangles];
+
+	for (int i = 0; i < numberOfTriangles; i++) {
+		indices[i * 3] = g_mesh.PointIndex(i).x;
+		indices[i * 3 + 1] = g_mesh.PointIndex(i).y;
+		indices[i * 3 + 2] = g_mesh.PointIndex(i).z;
+	}
+
+	g_pImmediateContext->Map(g_pVertexBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedVertexResource);
+	g_pImmediateContext->Map(g_pIndexBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &mappedIndexResource);
+
+	D3D11_BUFFER_DESC desc;
+	g_pIndexBuffer->GetDesc(&desc);
+	
+	memcpy(mappedVertexResource.pData, vertices, numberOfPoints * sizeof(SimpleVertex));
+	memcpy(mappedIndexResource.pData, indices, sizeof(WORD) * numberOfTriangles * 3);
+
+	g_pImmediateContext->Unmap(g_pVertexBuffer, 0);
+	g_pImmediateContext->Unmap(g_pIndexBuffer, 0);
+}
 
 //--------------------------------------------------------------------------------------
 // Render a frame
 //--------------------------------------------------------------------------------------
 void Render()
 {
+	int numberOfFrames = 30;
+	char baseName[256];
+	
     // Update our time
     static float t = 0.0f;
-    if( g_driverType == D3D_DRIVER_TYPE_REFERENCE )
-    {
-        t += ( float )XM_PI * 0.0125f;
-    }
-    else
-    {
-        static DWORD dwTimeStart = 0;
-        DWORD dwTimeCur = GetTickCount();
-        if( dwTimeStart == 0 )
-            dwTimeStart = dwTimeCur;
-        t = ( dwTimeCur - dwTimeStart ) / 1000.0f;
-    }
+	static int frameCnt = 0;
+
+	static DWORD dwTimeStart = 0;
+	DWORD dwTimeCur = GetTickCount();
+	if( dwTimeStart == 0 )
+		dwTimeStart = dwTimeCur;
+	t = ( dwTimeCur - dwTimeStart ) / 1000.0f;
+
+	float timeIntervalOfFrame = (float)1 / numberOfFrames;
+	
+	if (t > timeIntervalOfFrame) {
+		t -= timeIntervalOfFrame;
+		frameCnt++;
+
+		char baseName[256];
+		snprintf(baseName, sizeof(baseName), "frame_%06d.obj", frameCnt);
+		std::ifstream file(baseName);
+		g_mesh = TriangleMesh3();
+		g_mesh.ReadObj(&file);
+
+		UpdateBuffer();
+	}
+
+	// 
+	// My Camera
+	//
+
+	XMVECTOR At = XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f);
+	XMVECTOR Up = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+	g_View = XMMatrixLookAtLH(g_Eye, At, Up);
 
     //
     // Animate the cube
     //
-	g_World = XMMatrixRotationY( t );
+	g_World = XMMatrixRotationY( 0 );
 
     //
     // Clear the back buffer
     //
-    float ClearColor[4] = { 0.0f, 0.125f, 0.3f, 1.0f }; // red,green,blue,alpha
+    float ClearColor[4] = { 1.0f, 1.0f, 1.0f, 1.0f }; // red,green,blue,alpha
     g_pImmediateContext->ClearRenderTargetView( g_pRenderTargetView, ClearColor );
 
     //
@@ -526,8 +598,8 @@ void Render()
 	g_pImmediateContext->VSSetShader( g_pVertexShader, NULL, 0 );
 	g_pImmediateContext->VSSetConstantBuffers( 0, 1, &g_pConstantBuffer );
 	g_pImmediateContext->PSSetShader( g_pPixelShader, NULL, 0 );
-	g_pImmediateContext->DrawIndexed( 36, 0, 0 );        // 36 vertices needed for 12 triangles in a triangle list
-
+	g_pImmediateContext->DrawIndexed( g_mesh.NumberOfPoints(), 0, 0 );        // 36 vertices needed for 12 triangles in a triangle list
+	
     //
     // Present our back buffer to our front buffer
     //
